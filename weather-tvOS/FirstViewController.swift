d1c84be68d54c0b590b315e547a0968c7cee92aa
Joshua Hobson <hobson_joshua@yahoo.com>
2018-08-14 11:37:08 -0500

    Initial Commit

---
+++ weather-tvOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  weather-tvOS
+//
+//  Created by Joshua Hobson on 8/14/18.
+//  Copyright © 2018 Joshua Hobson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
